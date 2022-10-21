2c9f8f8b5db95d1f8c32a17a6eb1a7cb0ecbeb96
Jebu <95964955+Carbrex@users.noreply.github.com>
2022-10-21 15:49:17 +0000

    8.53.30 Project-15 cocktails

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem numquam delectus ea natus neque dolore aut provident sapiente? Esse quibusdam soluta accusamus eaque temporibus fuga delectus ea! Laudantium voluptate dignissimos expedita natus consequuntur magnam quae magni? Accusamus, tenetur. Obcaecati, cumque!
+      </p>
+    </section>
   )
 }
 
