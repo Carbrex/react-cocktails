25a3ca74c9cb792dced129da72f8fd84b88a5867
jebuAsus <95964955+Carbrex@users.noreply.github.com>
2023-04-14 18:04:54 +0530

    Changed a font color

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -19,7 +19,7 @@ const SearchForm = () => {
       <form className="w-5/6 my-0 mx-auto max-w-2xl bg-gray-800 py-8 px-10 capitalize rounded shadow-[2px_5px_3px_0px_rgba(0,0,0,0.5)]" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name" className='block mb-5 font-bold tracking-wide text-violet-300'>search your favorite cocktail</label>
-          <input type="text" id="name" ref={searchValue} className='w-full border-none border-transparent rounded p-2 text-xl' onChange={searchCocktail}/>
+          <input type="text" id="name" ref={searchValue} className='w-full border-none border-transparent rounded p-2 text-xl text-black' onChange={searchCocktail}/>
         </div>
       </form>
     </section>
